b3d3cdabff5eeb5f55c6fc12726c42a3aa665e2f
FarhadovaMaria <115582457+FarhadovaMaria@users.noreply.github.com>
2022-10-16 22:20:28 +0300

    Задания 6 и 9 с лабораторной 3 ( переделанные)

---
+++ lab3 new/Task9_new.py
@@ -0,0 +1,21 @@
+salary = 5000  # зарплата
+spend = 6000  # траты
+months = 10  # количество месяцев
+increase = 0.03  # рост цен
+
+
+
+
+
+
+
+need_money = 0  # количество денег, чтобы прожить 10 месяцев
+
+# TODO Оформить решение
+
+current_spend = spend
+for current_cup in range(1, months+1):
+    current_cup = (current_spend - salary)
+    current_spend = current_spend * (increase + 1)
+    need_money += current_cup
+print(round(need_money))
